3021a9dd9ce3fbdbd5490c5c9dc0e823d9f92640
Luis Anderson Davila Dominguez <andersondavila59@gmail.com>
2026-03-24 09:26:17 -0600

    mejore el mensaje para mi console.log

--- script.js
+++ script.js
@@ -1,11 +1,13 @@
 
 
-let nombre="GYM";
-let since=12;
-let abre=true;
-let cierra=false;
+let nombre = "GYM";
+let since = 12;
+let abre = true;
+let cierra = false;
 
-console.log(nombre+"CONTAMOS CON "+since+" EXPERIENCIA, ABIERTO LAS 24 HORAS DEL DIA "+bre+" NUNCA CIERRA "+cierra)
+console.log(nombre + " CONTAMOS CON " + since + " AÑOS DE EXPERIENCIA, " + (abre ? "ABIERTO LAS 24 HORAS DEL DÍA" : "NO ABIERTO") + ", " +(cierra ? "CIERRA" : "NUNCA CIERRA"));
+
+
 
 
 
