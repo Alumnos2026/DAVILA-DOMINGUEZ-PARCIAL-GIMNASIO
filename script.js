74a54a20558bfcd580b347b36d2a94dec0f38311
Luis Anderson Davila Dominguez <andersondavila59@gmail.com>
2026-03-24 09:29:13 -0600

    agregue la variable null y undefined

--- script.js
+++ script.js
@@ -4,8 +4,13 @@ let nombre = "GYM";
 let since = 12;
 let abre = true;
 let cierra = false;
+let especial = null;       
+let pendiente;            
 
-console.log(nombre + " CONTAMOS CON " + since + " AÑOS DE EXPERIENCIA, " + (abre ? "ABIERTO LAS 24 HORAS DEL DÍA" : "NO ABIERTO") + ", " +(cierra ? "CIERRA" : "NUNCA CIERRA"));
+console.log(nombre + " CONTAMOS CON " + since + " AÑOS DE EXPERIENCIA, " +(abre ? "ABIERTO LAS 24 HORAS DEL DÍA" : "NO ABIERTO") + ", " +(cierra ? "CIERRA" : "NUNCA CIERRA") + " | Valor especial: " + especial + " | Valor pendiente: " + pendiente
+);
+
+
 
 
 
